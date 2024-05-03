79289eb4e0c8557eceb30281c04a49ad7c70f06e
Landtree <delandtree@gmail.com>
2024-05-03 12:19:54 -0700

    update

--- include/Pulse.h
+++ include/Pulse.h
@@ -1,23 +1,32 @@
+#pragma once
+#include <stdint.h>
+#include <avr/io.h>
 #ifndef PULSE_H
 #define PULSE_H
+
+#include <Timer.h>
+
 
 class Pulse 
 {   
     public:
         Pulse();
-        void attach(int PWMpin);
-        void setRate(int rate);
+        void attach(int8_t PWMpin);
+        void setRate(int8_t rate);
         void update(bool pulseEnable);
-        void setMax(int maxVal);
-        void setMin(int minVal);
+        void setMax(int8_t maxVal);
+        void setMin(int8_t minVal);
+        void setIncrement(int8_t increment);
     private:
-        int PWMpin;
+        int8_t PWMpin;
         bool pulseEnable;
-        int rate;
-        int maxVal = 255; //max value to raise light to
-        int minVal = 0; //min value to lower light to
-        int increment = 5; //how many increments to increase
+        int8_t rate;
+        int8_t maxVal = 255; //max value to raise light to
+        int8_t minVal = 0; //min value to lower light to
+        int8_t increment = 5; //how many increments to increase
         bool En = 0; //bool to hold direction of pulse
+        MoToTimer lightRaise; // Declare lightRaise as a member variable
+        MoToTimer lightLower; // Declare lightLower as a member variable
 
 };
 
--- src/Pulse.cpp
+++ src/Pulse.cpp
@@ -4,36 +4,65 @@ This uses the GPL-3.0 Licence.
 It uses the MoToTimer from the MoBaTool library for timing.
 https://github.com/MicroBahner/MobaTools
 
+***Using the modified OMSI Timer that is a stripped down version of Mobatools*****
+
+
 */
 
+
 #include <Pulse.h>
-#include <MoToTimer.h>
-MoToTimer lightRaise;
-MoToTimer lightLower;
 
 Pulse::Pulse()
 {
-
+    // Create a new MoToTimer object for light raising
+    lightRaise = MoToTimer();
+    lightLower = MoToTimer();
 }
 
-void Pulse::attach(int PWMpin)
+void Pulse::attach(int8_t PWMpin)
 {   
-    Pulse::PWMpin = PWMpin;    
+    Pulse::PWMpin = PWMpin; 
+    setupPWM();   
 }
 
-void Pulse::setRate(int rate)
+void Pulse::setRate(int8_t rate)
 {
     Pulse::rate = rate;
 }
 
-void Pulse::setMax(int maxVal)
+void Pulse::setMax(int8_t maxVal)
 {
     Pulse::maxVal = maxVal;
 }
 
-void Pulse::setMin(int minVal)
+void Pulse::setMin(int8_t minVal)
 {
     Pulse::minVal = minVal;
+}
+
+void Pulse::setIncrement(int8_t increment)
+{
+    Pulse::increment = increment;
+}
+
+
+void writePWM(uint8_t pin, uint8_t value) {
+    // Map value (0-255) to PWM duty cycle (0-65535)
+    uint16_t dutyCycle = (uint16_t)value * 256;
+
+    // Select the appropriate timer and output compare register based on the pin
+    switch (pin) {
+        case 0: // Example pin, replace with the actual pin number on your ItsyBitsy
+            // Configure Timer0 for Fast PWM mode, non-inverting
+            TCCR0A |= (1 << WGM00) | (1 << WGM01) | (1 << COM0A1);
+            TCCR0B |= (1 << CS00); // No prescaling
+            OCR0A = dutyCycle;
+            break;
+        // Add cases for other pins as needed
+        default:
+            // Unsupported pin
+            break;
+    }
 }
 
 void Pulse::update(bool pulseEnable)
@@ -46,7 +75,7 @@ void Pulse::update(bool pulseEnable)
                 if(En == 0)
                 {   
                     increment++;
-                    analogWrite(PWMpin, increment);
+                    writePWM(PWMpin, increment);
                     lightRaise.setTime(rate);                    
                     if(increment == maxVal){En = 1;}      
                 }
@@ -55,7 +84,7 @@ void Pulse::update(bool pulseEnable)
                 if(En == 1)
                 {  
                     increment--; 
-                    analogWrite(PWMpin, increment);
+                    writePWM(PWMpin, increment);
                     lightRaise.setTime(rate);
                     if(increment == minVal){En = 0;}            
                 }
@@ -69,7 +98,7 @@ void Pulse::update(bool pulseEnable)
                 if(increment > 0)
                 {
                 increment--; 
-                analogWrite(PWMpin, increment);
+                writePWM(PWMpin, increment);
                 lightLower.setTime(rate);
                 lightLower.restart();
                 }
